f7d81a550b04737315f17b3433f004056e5cae44
jitasek <jitka.moreira@cdn77.com>
2022-04-13 11:00:10 +0200

    Add _document.js and method to fetch data from a 3rd party API

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
 import Head from "next/head"; // this is used for custom titles, meta tags, keywords, descriptions
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
         <title>Web Dev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to Next</h1>
+
+      {articles.map((article) => (
+        <h3>{article.title}</h3>
+      ))}
     </div>
   );
 }
+// getStaticProps method allows us to fetch at build time from this 3rd party fake API called JSON Placeholder which just gets a bunch of posts for us
+// I am limiting the scope to 6
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json(); // gives us the data, await for it too
+
+  // returns an object with props; inside the props is the data I wanna pass as props, i.e. articles I wanna fetch - need to pass them in the Home component too!
+  return {
+    props: {
+      articles,
+    },
+  };
+};
