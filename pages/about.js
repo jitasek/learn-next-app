ec34ba05819a2c0446707efd1d81e49ebf094f1e
jitasek <jitka.moreira@cdn77.com>
2022-04-14 15:50:41 +0200

    Override 2 default meta titles and 1 description

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 // import Head from "next/head";
+import Meta from "../components/Meta"; // I import it to be able to change the meta title for this specific page (see below)
 
 const about = () => {
   return (
     <div>
+      <Meta title="About" />
       {/* <Head>
         <title>About</title>
       </Head> */}
