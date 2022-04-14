e1da15c738d5bfe88461249c2f7f09151df7adfa
jitasek <jitka.moreira@cdn77.com>
2022-04-14 14:53:54 +0200

    Fetch data from the correct paths + config server

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from "../../../config/index";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -17,10 +18,37 @@ const article = ({ article }) => {
 
 // getServerSideProps and getStaticProps can get passed in a context - that allows me to get the id of whatever is in the URL
 // if we use getStaticProps, we need also the getStaticPaths (for getServerSideProps we don't )
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   const articles = await res.json();
+
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } })); // for each id we wanna return an object with params, inside params we have another object with id which equals the id but we need to format it as a string
+
+//   return {
+//     // paths: { params: { id: "1", id: "2" } }, // to be able to do this programatically, I need first to get an array of id of all the articles(posts) - see the above two map methods
+//     paths,
+//     fallback: false, // this will return 404 if user goes to some url that doesn't exist
+//   };
+// };
+
+// export default article;
+
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
   const article = await res.json();
 
   return {
@@ -31,7 +59,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   const ids = articles.map((article) => article.id);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { server } from "../config";
 import Head from "next/head"; // this is used for custom titles, meta tags, keywords, descriptions
 import ArticleList from "../components/ArticleList";
 
@@ -16,13 +17,23 @@ export default function Home({ articles }) {
 }
 // getStaticProps method allows us to fetch at build time from this 3rd party fake API called JSON Placeholder which just gets a bunch of posts for us
 // I am limiting the scope to 6
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
-  const articles = await res.json(); // gives us the data, await for it too
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json(); // gives us the data, await for it too
 
-  // returns an object with props; inside the props is the data I wanna pass as props, i.e. articles I wanna fetch - need to pass them in the Home component too!
+//   // returns an object with props; inside the props is the data I wanna pass as props, i.e. articles I wanna fetch - need to pass them in the Home component too!
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
   return {
     props: {
       articles,
